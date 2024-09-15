3554d58f383f6e3f03a885b9504fc977beb865c4
Apollo Nargang <CoffeeCoder1@outlook.com>
2024-09-15 18:27:59 -0400

    Create a CI artifact that includes the demo Initial work towards what could potentially become an archive for the Godot asset store.

--- ci/package.bash
+++ ci/package.bash
@@ -1,3 +1,7 @@
 cp $1/*/lib/*.so    addons/pure-data/
 cp $1/*/lib/*.dll   addons/pure-data/
 # cp $1/*/lib/*.dylib addons/pure-data/
+
+mkdir -p work/dist
+cp -R addons work/dist/
+cp -R demo work/dist/
